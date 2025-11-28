573449ae780b9a980f69966247e139278cc1a465
TinyRaindrop <48414047+TinyRaindrop@users.noreply.github.com>
2025-11-28 19:17:34 +0200

    Unify resource loading logic. Pass loaded terms to subprocesses.

--- run_app.py
+++ run_app.py
@@ -1,3 +1,25 @@
+# #nuitka-project: --onefile
+# nuitka-project: --standalone
+# nuitka-project: --output-filename=fictionpub.exe
+# nuitka-project: --output-dir=./dist/
+
+# nuitka-project: --windows-console-mode=force
+# (disable/force/attach/hide)
+
+# nuitka-project: --lto=yes
+# nuitka-project: --assume-yes-for-downloads
+
+# nuitka-project: --enable-plugin=tk-inter
+
+# include resource folder
+# nuitka-project: --include-package-data=fictionpub.resources
+# nuitka-project: --include-data-dir=fictionpub/resources=fictionpub/resources
+
+# nuitka-project: --static-libpython=auto
+# nuitka-project: --follow-imports
+
+# nuitka-project: --mingw64
+
 """
 Entry point for Pyinstaller.
 """
